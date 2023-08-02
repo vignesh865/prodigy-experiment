81628d3feb8858c0599d8bef2a3b7f7c34feffa7
Vignesh Baskaran <vignesh865@gmail.com>
2023-08-02 15:56:38 +0530

    Add files via upload

---
+++ Prodigy/demo-files/download_demo_files.py
@@ -0,0 +1,5 @@
+import gdown
+
+url = 'https://drive.google.com/file/d/1Q6nbtWnZNw7_THOa0UgtqiB_xntTFzY9/view?usp=sharing'
+output = 'hgh.pdf'
+gdown.download(id="1Q6nbtWnZNw7_THOa0UgtqiB_xntTFzY9", output= output, quiet=False)
